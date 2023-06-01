ff34cc095eec062efb9f0a674047c6c73a2ba6df
cofdream <cofdream@sina.com>
2023-06-01 23:04:41 +0800

    add learn Blazor Server

---
+++ BlazorApp/wwwroot/mapComponent.js
@@ -0,0 +1,16 @@
+﻿import 'https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY29mZHJlYW0iLCJhIjoiY2xpZDluM3V2MDUzczNlcWpkYjUxa3N5YiJ9.tGQC3lMP0QgHtFrTywIPPw';
+
+export function addMapToElement(element) {
+    return new mapboxgl.Map({
+        container: element,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-74.5, 40],
+        zoom: 9
+    });
+}
+
+export function setMapCenter(map, latitude, longitude) {
+    map.setCenter([longitude, latitude]);
+}
