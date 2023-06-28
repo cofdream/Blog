683a99d13bd4f593d46a177185407ec49148850e
cofdream <cofdream@sina.com>
2023-06-28 19:01:39 +0800

    update

--- BlazorApp/Service/BlogService.cs
+++ BlazorApp/Service/BlogService.cs
@@ -1,34 +1,47 @@
-﻿using BlazorApp.Data;
+﻿using System.IO;
+using BlazorApp.Data;
 using BlazorApp.Shared;
 using System.Linq;
+using YamlDotNet.Serialization;
 
 namespace BlazorApp.Service
 {
-	public class BlogService
-	{
-		private readonly BlogPost[] _blogPosts = new BlogPost[]
-		{
-			new BlogPost{ URL="1",Title = "No.1 Post" , Description = "one blog"},
-			new BlogPost{ URL="2",Title = "No.2 Post" , Description = "two blog"},
-			new BlogPost{ URL="3",Title = "No.2 Post" , Description = "three blog"},
-		};
+    public class BlogService
+    {
+        private readonly BlogPost[] _blogPosts = new BlogPost[]
+        {
+            new BlogPost{ URL="1",Title = "No.1 Post Title" , Description = "one blog", Content = "content *test*"},
+            new BlogPost{ URL="2",Title = "No.2 Post Title" , Description = "two blog"},
+            new BlogPost{ URL="3",Title = "No.2 Post Title" , Description = "three blog"},
+        };
 
-		public BlogPost[] GetBlogPosts()
-		{
-			return _blogPosts;
-		}
+        public BlogService()
+        {
+            var blogPostsPath = $"{Directory.GetCurrentDirectory()}/wwwroot/BlogPosts";
+            var files = Directory.GetFiles(blogPostsPath, "*.md", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                Console.WriteLine(File.ReadAllText(file));
+                Console.WriteLine("------------------------------------------------");
+            }
+        }
 
-		public BlogPost GetBlogPost(string url)
-		{
-			foreach (var blogPost in _blogPosts)
-			{
-				if (blogPost.URL == url)
-				{
-					return blogPost;
-				}
-			}
+        public BlogPost[] GetBlogPosts()
+        {
+            return _blogPosts;
+        }
 
-			return null;
-		}
-	}
+        public BlogPost GetBlogPost(string url)
+        {
+            foreach (var blogPost in _blogPosts)
+            {
+                if (blogPost.URL == url)
+                {
+                    return blogPost;
+                }
+            }
+
+            return null;
+        }
+    }
 }
--- BlazorApp/wwwroot/BlogPosts/about.md
+++ BlazorApp/wwwroot/BlogPosts/about.md
@@ -1,12 +1,3 @@
----
-title: '关于'
-date: 2022-09-01 16:34:56
-tags: []
-published: true
-hideInList: true
-feature: 
-isTop: false
----
 > 欢迎来到我的小站呀，很高兴遇见你！🤝
 
 ## 🏠 关于本站
