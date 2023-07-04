53bd194ac1ade02b6eb099c6b7e4c9a319572665
cofdream <cofdream@sina.com>
2023-07-04 18:51:20 +0800

    update

--- BlazorApp/Data/BlogPost.cs
+++ BlazorApp/Data/BlogPost.cs
@@ -4,10 +4,11 @@ namespace BlazorApp.Data
 {
 	public class BlogPost
 	{
-		public string Url = string.Empty;
-		public string[] Tags = Array.Empty<string>();
-		public string Title = string.Empty;
-		public string Description = string.Empty;
-		public DateTime DateTime = DateTime.Now;
-	}
+        public string Url = string.Empty;
+        public string[] Tags = Array.Empty<string>();
+        public string Title = string.Empty;
+        public string Description = string.Empty;
+        public string Content = string.Empty;
+        public DateTime DateTime = DateTime.Now;
+    }
 }
